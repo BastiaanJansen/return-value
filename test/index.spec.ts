b7e1f7a82b537316a8d270559f94604c5124d45d
newo2001 <JoGamesVideos@gmail.com>
2021-01-27 10:52:21 +0100

    updated license in package.json and added 2 new tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -10,4 +10,12 @@ describe("return value test cases", () => {
 	it("should return number value", () => {
 		expect(returnValue<number>(1)).to.equal(1).and.to.be.a("number");
 	});
+
+	it("should return a a jojo's reference", () => {
+		expect(returnValue("ほう... 向かってくるのか")).to.equal("ほう... 向かってくるのか").and.to.be.a("string");
+	});
+
+	it("should return a poop emoji", () => {
+		expect(returnValue('💩')).to.equal('💩').and.to.be.a("string");
+	});
 });
